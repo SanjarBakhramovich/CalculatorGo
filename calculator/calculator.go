698010dceeec53d534ea9209fe70a78506bb3363
Sanjar <sanjarbakhramovich@gmail.com>
2023-11-29 12:24:03 +0500

    fix secondValue

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -32,23 +32,27 @@ func (c *Calculator) ParseInput() {
 	secondValueStr := parts[2]
 
 	firstValue, err := roman.RomanToArabic(firstValueStr)
+	// check if
+	// esli firstValue = Roman
+	// Prebrazovat v arabic
+	//
 	if err != nil {
 		floatValue, err := strconv.ParseFloat(firstValueStr, 32)
 		if err != nil {
 			fmt.Println("Ошибка при преобразовании первого значения", err)
 			return
 		}
-		firstValue = uint8(floatValue)
+		firstValue = float32(floatValue)
 	}
 
 	secondValue, err := roman.RomanToArabic(secondValueStr)
 	if err != nil {
-		floatValue, err := strconv.ParseFloat(firstValueStr, 32)
+		floatValue, err := strconv.ParseFloat(secondValueStr, 32)
 		if err != nil {
 			fmt.Println("Ошибка при преобразовании второго значения", err)
 			return
 		}
-		secondValue = uint8(floatValue)
+		secondValue = float32(floatValue)
 	}
 
 	c.MathOperation = parts[1]
@@ -61,6 +65,7 @@ func NewCalculator(input string) *Calculator {
 	calculator.ParseInput()
 	return calculator
 }
+
 func (c *Calculator) Calculate() float32 {
 	switch c.MathOperation {
 	case Sum:
@@ -70,6 +75,10 @@ func (c *Calculator) Calculate() float32 {
 	case Multiplication:
 		return c.FirstValue * c.SecondValue
 	case Division:
+		if c.SecondValue == 0 {
+			fmt.Println("Ошибка деление на ноль")
+			return 0
+		}
 		return c.FirstValue / c.SecondValue
 	default:
 		fmt.Println("Неверная математическая операция")
--- roman/roman.go
+++ roman/roman.go
@@ -2,8 +2,8 @@ package roman
 
 import "errors"
 
-func RomanToArabic(roman string) (uint8, error) {
-	romanNums := map[string]uint8{
+func RomanToArabic(roman string) (float32, error) {
+	romanNums := map[string]float32{
 		"I": 1, "II": 2, "III": 3, "IV": 4,
 		"V": 5, "VI": 6, "VII": 7, "VIII": 8,
 		"IX": 9,
